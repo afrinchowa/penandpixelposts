66dd187607e6be52b52e6a2a07f4b06af070216d
Chowa <arfinchowa524@gmail.com>
2024-03-15 13:52:47 +0600

    Proptype setting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-
+import profile from '../../assets/p1.jpg';
 
 const Header = () => {
     return (
-        <div>
+        <header className="flex justify-between items-center p-4 mx-4 border-b-2" >
             <h1 className="text-4xl font-bold">PenAndPixelPosts</h1>
-        </div>
+            <img className='w-10 h-14 rounded-full'  src={profile} alt="" />
+        </header>
     );
 };
 
